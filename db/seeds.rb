571d6f3440c636e74915c2ad008f91da140f7f81
Kim North <mickeymirage@hotmail.com>
2017-06-21 21:11:48 +0100

    Moving sqlite gem to development in gemfile, adding pg gem to gemfile. Fixing seeds file to populate the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+user1 = User.new(email: "email1@email.com", password: "123456")
+user1.save
+
+user2 = User.new(email: "email2@email.com", password: "123456")
+user2.save
+
+user3 = User.new(email: "email3@email.com", password: "123456")
+user3.save

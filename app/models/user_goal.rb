02944ead0e17d4334c5ac9f1d47c324188c6571c
Kim North <mickeymirage@hotmail.com>
2017-06-22 16:19:36 +0100

    Commiting before heroku upload.

--- app/models/user_goal.rb
+++ app/models/user_goal.rb
@@ -1,6 +1,6 @@
 class UserGoal < ApplicationRecord
 	belongs_to :user
-
 	validates :user_id, presence: true, uniqueness: true
-
+	validates :target, presence: true
+	validates :current, presence: true
 end
